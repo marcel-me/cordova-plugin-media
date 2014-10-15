dd3b5b5df9df3975aadba010dc5304c7912c6f8d
Marcel Medak <marcel.medak@softwareinmotion.de>
2014-10-15 16:41:04 +0200

    use duck others option for playback

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -326,7 +326,7 @@
                 }
 
                 NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayback : AVAudioSessionCategorySoloAmbient;
-                [self.avSession setCategory:sessionCategory error:&err];
+                [self.avSession setCategory:sessionCategory withOptions:AVAudioSessionCategoryOptionDuckOthers error:&err];
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
                     NSLog(@"Unable to play audio: %@", [err localizedFailureReason]);
